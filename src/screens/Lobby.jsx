319e40dafa9bc88f4685ccf72909ea8a85a4fe83
sunil <sunilahuja1702@gmail.com>
2025-04-15 17:52:51 +0530

    added domain

--- src/screens/Lobby.jsx
+++ src/screens/Lobby.jsx
@@ -44,7 +44,7 @@ const LobbyScreen = () => {
             <input
               type="email"
               id="email"
-              className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              className="w-full px-4 py-2 text-black border border-gray-300 dark:border-gray-700 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
